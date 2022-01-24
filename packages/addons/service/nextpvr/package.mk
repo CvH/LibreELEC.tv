025a15dc0cc2331ee1c12c3f664843959bdbfe57
CvH <namerp@googlemail.com>
2022-01-24 12:29:49 +0100

    nextpvr: add openssl-compat

--- packages/addons/service/nextpvr/package.mk
+++ packages/addons/service/nextpvr/package.mk
@@ -7,7 +7,7 @@ PKG_REV="100"
 PKG_ARCH="any"
 PKG_LICENSE="prop."
 PKG_SITE="https://nextpvr.com"
-PKG_DEPENDS_TARGET="toolchain"
+PKG_DEPENDS_TARGET="toolchain openssl-compat"
 PKG_SECTION="service"
 PKG_SHORTDESC="NextPVR Server"
 PKG_LONGDESC="NextPVR is a personal video recorder application. It allows to watch or record live TV, provides great features like series recordings and web scheduling."
@@ -20,5 +20,6 @@ PKG_ADDON_PROJECTS="any"
 PKG_ADDON_REQUIRES="tools.ffmpeg-tools:0.0.0 tools.dotnet-runtime:0.0.0"
 
 addon() {
-  :
+  mkdir -p ${ADDON_BUILD}/${PKG_ADDON_ID}/lib
+  cp -PL $(get_install_dir openssl-compat)/usr/local/lib/{libcrypto.so.1.1,libssl.so.1.1} ${ADDON_BUILD}/${PKG_ADDON_ID}/lib
 }
