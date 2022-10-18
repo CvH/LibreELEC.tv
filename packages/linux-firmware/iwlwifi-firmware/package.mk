1acd1773d89d270f06b2f6b1c7e3da6e96857738
LibreELECBot <LibreELECBot@users.noreply.github.com>
2022-10-18 09:42:14 +0000

    Update dependencies

--- packages/linux-firmware/iwlwifi-firmware/package.mk
+++ packages/linux-firmware/iwlwifi-firmware/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="iwlwifi-firmware"
-PKG_VERSION="9c65a729599eda5cd4ffcae6acc64e00d2f8a058"
-PKG_SHA256="d34bb99563f60ce6a1c1ca645da0a9dbf476a21230c58f5353c5db4472d5ed4e"
+PKG_VERSION=""
+PKG_SHA256=""
 PKG_LICENSE="Free-to-use"
 PKG_SITE="https://github.com/LibreELEC/iwlwifi-firmware"
 PKG_URL="https://github.com/LibreELEC/iwlwifi-firmware/archive/${PKG_VERSION}.tar.gz"
