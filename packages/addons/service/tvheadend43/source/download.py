1913a9b3d874806862cd6bf1eda25638d560b2c5
CvH <namerp@googlemail.com>
2023-11-10 16:52:10 +0100

    121

--- packages/addons/service/tvheadend43/source/download.py
+++ packages/addons/service/tvheadend43/source/download.py
@@ -3,6 +3,7 @@ import shutil
 import urllib.request
 import subprocess
 
+
 def clear_directory(directory):
     try:
         for file_name in os.listdir(directory):
@@ -15,22 +16,28 @@ def clear_directory(directory):
     except Exception as e:
         print(f"Error clearing contents of '{directory}': {e}")
 
+
 def download_and_extract(url, destination, extract_path):
     try:
         # Download the file
         urllib.request.urlretrieve(url, destination)
 
         # Extract the file to the specified directory, ignoring the root path
-        subprocess.run(['tar', 'xf', destination, '--strip-components=3', '-C', extract_path])
+        subprocess.run(
+            ["tar", "xf", destination, "--strip-components=3", "-C", extract_path]
+        )
 
         print(f"Successfully downloaded and extracted to '{extract_path}'.")
     except Exception as e:
         print(f"Error downloading or extracting: {e}")
 
+
 if __name__ == "__main__":
-    scan_tables_path = '/storage/pp/99'
-    download_url = 'https://linuxtv.org/downloads/dtv-scan-tables/dtv-scan-tables-LATEST.tar.bz2'
-    downloaded_file_path = '/tmp/dtv-scan-tables-LATEST.tar.bz2'
+    scan_tables_path = "/storage/pp/99"
+    download_url = (
+        "https://linuxtv.org/downloads/dtv-scan-tables/dtv-scan-tables-LATEST.tar.bz2"
+    )
+    downloaded_file_path = "/tmp/dtv-scan-tables-LATEST.tar.bz2"
 
     # Clear the contents of the directory
     clear_directory(scan_tables_path)
