d4f89f3854c829fe8092414e46af26106fbb35e2
CvH <namerp@googlemail.com>
2026-05-10 16:22:39 +0200

    d

---
+++ packages/wayland/util/wlrctl/package.mk
@@ -0,0 +1,11 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2026-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="wlrctl"
+PKG_VERSION="0.2.2"
+PKG_SHA256="b5b3836091c41de09a832df1e5eb4747841d7ae670367e413487d5be7a5f2849"
+PKG_LICENSE="MIT"
+PKG_SITE="https://git.sr.ht/~brocellous/wlrctl"
+PKG_URL="https://git.sr.ht/~brocellous/wlrctl/archive/v${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain wayland wayland-protocols"
+PKG_LONGDESC="Utility for miscellaneous wlroots Wayland extensions."
